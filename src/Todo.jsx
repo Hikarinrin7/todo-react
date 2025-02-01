4584cc8dbf25982e2c035042100f7ba35d17532d
Hikarinrin7 <shizuoka_iitoko@yahoo.co.jp>
2025-02-01 11:55:23 +0900

    htmlマークアップまで

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
+// ここで読み込むと、全部にかかってしまう
+import { useState } from 'react';
 import './style.css'
 
 export const Todo = ()=> {
-
   return (
     <>
-      <div>
+      {/* Reactのjsx上でclassとしてしまうと別の意味合いになってしまう。classNameで */}
+      <div className='input-area'>
         <input placeholder='Todoを入力' />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のtodo</p>
+      <div className='incomplete-area'>
+        <p className='title'>未完了のtodo</p>
         <ul>
           <li>
-            <p>todoです</p>
-            <button>完了</button>
-            <button>削除</button>
+            <div className='list-row'>
+              <p className='todo-item'>todoです</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
           </li>
         </ul>
       </div>
-      <div>
-      <p>完了のtodo</p>
+      <div className='complete-area'>
+      <p className='title'>完了のtodo</p>
         <ul>
           <li>
-            <p>todoでした</p>
-            <button>戻す</button>
-          </li>
-          <li>
-            <p>todoでした</p>
-            <button>戻す</button>
+            <div className='list-row'>
+              <p className='todo-item'>todoでした</p>
+              <button>戻す</button>
+            </div>
           </li>
         </ul>
       </div>
