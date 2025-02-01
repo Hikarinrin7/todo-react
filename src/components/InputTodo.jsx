e0b5c5ec7addd27642bbb13cbce2f43eb236e75f
Hikarinrin7 <shizuoka_iitoko@yahoo.co.jp>
2025-02-01 17:42:22 +0900

    cssもコンポーネント内に書いてみよう（一部）

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,8 +1,17 @@
+const style={
+    backgroundColor: "#c6e5d9",
+    width: "400px",
+    height: "30px",
+    padding: "8px",
+    margin: "8px",
+    borderRadius: "8px",
+}
+
 export const InputTodo = (props) => {
     const { todoText, onChange, onClick } = props;
     return(
         // Reactのjsx上でclassとしてしまうと別の意味合いになってしまう。classNameで
-        <div className='input-area'>
+        <div style={style}>
             <input 
                 placeholder='Todoを入力'
                 value={todoText}
