70a643a2520a5e82ff2e270170a107058d3ad178
Hikarinrin7 <shizuoka_iitoko@yahoo.co.jp>
2025-02-01 11:41:43 +0900

    htmlを仮置き

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
-# React + Vite
-
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+# todoアプリ by Javascript
+## Udemy React講座 Section5
+https://www.udemy.com/share/103Fxl3@DzAZwZvvPOIycWga7GvhLadTPbK9We6P6ThQVn12vg-Jp1aZ9ekJ9AuC5D15A0zTCA==/
---
+++ src/Todo.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import './style.css'
+
+export const Todo = ()=> {
+
+  return (
+    <>
+      <div>
+        <input placeholder='Todoを入力' />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のtodo</p>
+        <ul>
+          <li>
+            <p>todoです</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+        </ul>
+      </div>
+      <div>
+      <p>完了のtodo</p>
+        <ul>
+          <li>
+            <p>todoでした</p>
+            <button>戻す</button>
+          </li>
+          <li>
+            <p>todoでした</p>
+            <button>戻す</button>
+          </li>
+        </ul>
+      </div>
+    </>
+    
+  );
+}
+
